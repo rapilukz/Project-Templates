31f918a13d8f98b1d475fbf8fcba70b8164bcc89
rapilukz <83957316+rapilukz@users.noreply.github.com>
2022-10-04 19:58:52 +0100

    Updated discord-bot-ts

--- templates/discord-bot-ts/src/index.ts
+++ templates/discord-bot-ts/src/index.ts
@@ -1,16 +1,14 @@
 import Client from './Client';
-import { Intents } from 'discord.js';
+import { GatewayIntentBits } from 'discord.js';
 
-const myIntens = new Intents();
-myIntens.add(
-  Intents.FLAGS.GUILDS,
-  Intents.FLAGS.GUILD_MESSAGES,
-  Intents.FLAGS.GUILD_MEMBERS,
-  Intents.FLAGS.GUILD_INTEGRATIONS,
-  Intents.FLAGS.DIRECT_MESSAGES,
-  Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
-  Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-  Intents.FLAGS.GUILD_MESSAGE_TYPING,
-);
-
-new Client({intents: myIntens }).init();
+new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildIntegrations,
+    GatewayIntentBits.GuildEmojisAndStickers,
+    GatewayIntentBits.DirectMessageReactions,
+    GatewayIntentBits.GuildMessageTyping,
+  ],
+}).init();
